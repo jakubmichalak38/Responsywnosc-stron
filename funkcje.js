6103a55408b8d38cef1fe2b1fc11aa99c8770de5
jakubmichalak38 <124298386+jakubmichalak38@users.noreply.github.com>
2024-01-11 22:55:20 +0100

    Add files via upload

---
+++ funkcje.js
@@ -0,0 +1,27 @@
+function WinOpen_z2()
+{
+window.open("zadanie2.html","okienko_2","toolbar=no,directories=no,menubar=no,height=400,width=840,top=200,left=200");
+}
+function WinOpen_z3()
+{
+window.open("zadanie3.html","okienko_3","toolbar=no,directories=no,menubar=no,height=400,width=840,top=200,left=200");
+}
+function WinOpen_z4()
+{
+window.open("zadanie4.html","okienko_4","toolbar=no,directories=no,menubar=no,height=400,width=840,top=200,left=200");
+}
+
+/*Funkcje kody*/
+
+function WinOpen_z2KOD()
+{
+window.open("zadanie2KOD.html","okienko_2KOD","toolbar=no,directories=no,menubar=no,height=400,width=440,top=200,left=200");
+}
+function WinOpen_z3KOD()
+{
+window.open("zadanie3KOD.html","okienko_3KOD","toolbar=no,directories=no,menubar=no,height=400,width=440,top=200,left=200");
+}
+function WinOpen_z4KOD()
+{
+window.open("zadanie4KOD.html","okienko_4KOD","toolbar=no,directories=no,menubar=no,height=400,width=560,top=200,left=200");
+}
